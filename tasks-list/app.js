8862ec7f1f0daa4a0b211b57122b07114943c13f
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-09-07 20:16:40 +0200

    add prueba editar

--- tasks-list/app.js
+++ tasks-list/app.js
@@ -1,3 +1,23 @@
+// Local Storage
+// const list = []
+
+// if (!localStorage.getItem('list')) {
+//     localStorage.setItem('list', []);
+// }
+
+// function getList() {
+//     let list = localStorage.getItem('list');
+
+//     counter++;
+//     document.querySelector('h1').innerHTML = counter;
+//     localStorage.setItem('counter', counter)
+
+//     if (counter % 10 === 0) {
+//         alert(`Count is now ${counter}`)
+//     }
+// }
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // Elements
     const taskInput = document.querySelector('#task-input');
@@ -69,4 +89,27 @@ document.addEventListener('DOMContentLoaded', () => {
     function editTask(e) {
         let task = e.target.parentNode.parentNode;
     }
+
+    /* 
+    document.addEventListener("DOMContentLoaded", function () {
+      const listaTareas = document.getElementById("listaTareas");
+      listaTareas.addEventListener("click", function (e) {
+        if (e.target.classList.contains("editar")) {
+          const tarea = e.target.parentElement;
+          const textoTarea = tarea.firstChild.textContent.trim();
+          const nuevoTexto = prompt("Editar tarea:", textoTarea);
+    
+          if (nuevoTexto !== null && nuevoTexto !== "") {
+            tarea.firstChild.textContent = nuevoTexto;
+          }
+        }
+      });
+    });
+    
+    */
+
+
+
+
+
 });

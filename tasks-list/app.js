6feeccbf798cd959aafaa301c40fad2ef7f70c6a
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-09-17 11:41:13 +0200

    fixed LocalStorage, fix style

--- tasks-list/app.js
+++ tasks-list/app.js
@@ -34,7 +34,7 @@ const addNewTask = (id, task, done, eliminated) => {
     if (eliminated) { return }
     const DONE = done ? 'done' : '';
     const element = `<div class="task roundBorder ${DONE}">
-                        <p contenteditable="false" data="editable">${task}</p>
+                        <p id="${id}" contenteditable="false" data="editable">${task}</p>
                         <div class="icons">
                             <i id="${id}" data="done" class="${check}"></i>
                             <i id="${id}" data="eliminated" class="${trash}"></i>
@@ -44,13 +44,13 @@ const addNewTask = (id, task, done, eliminated) => {
 }
 
 const changeTaskState = element => {
-    console.log(element.parentNode.parentNode.classList)
+    // console.log(element.parentNode.parentNode.classList)
     element.parentNode.parentNode.classList.toggle('done');
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 const eliminateTask = element => {
-    console.log(element.parentNode.parentNode);
+    // console.log(element.parentNode.parentNode);
     element.parentNode.parentNode.remove();
 
     LIST[element.id].eliminated = true;
@@ -62,8 +62,6 @@ const editTask = element => {
         task.classList.add('editing');
         task.contentEditable = true;
         task.focus()
-
-        LIST[element.id].task = task.innerText;
     }
 }
 
@@ -73,8 +71,9 @@ const focusOut = element => {
         task.contentEditable = false;
         task.classList.remove('editing');
 
-        console.log(`Con focus out: ${task.innerText}`);
-        LIST[element.id].task = task.innerText;
+        // console.log(`Con focus out: ${task.innerText}`);
+
+        LIST[task.id].task = task.innerText;
 
     } else {
         task.innerText = "It can't be an empty task"
@@ -83,83 +82,14 @@ const focusOut = element => {
 }
 
 const skipJump = element => {
-    // element.preventDefault();
     const task = element;
     task.blur();
     task.contentEditable = false;
     task.classList.remove('editing');
 
-    console.log(`Con enter: ${task.innerText}`);
-    LIST[element.id].task = task.innerText;
+    // console.log(`Con enter: ${LIST[element.id].task}`);
+
 }
-
-
-// const changeTaskState = event => {
-//     event.target.parentNode.parentNode.classList.toggle('done');
-// }
-
-
-// const addNewTask = event => {
-//     event.preventDefault();
-//     const { value } = event.target.taskInput
-//     if (!value) return;
-//     const task = document.createElement('div')
-//     task.setAttribute('id', id);
-
-//     id++;
-
-//     task.classList.add('task', 'roundBorder');
-//     // task.addEventListener('click', changeTaskState)
-
-//     const taskText = document.createElement('p')
-//     taskText.textContent = value;
-
-//     // Edit (Ver como sacar esto de acá)
-//     taskText.addEventListener('dblclick', editTask);
-//     taskText.addEventListener('focusout', focusOut);
-//     taskText.addEventListener('keydown', skipJump);
-
-//     task.appendChild(taskText)
-
-//     let icons = document.createElement('div');
-//     icons.classList.add('icons');
-
-//     task.appendChild(icons);
-
-//     let done = document.createElement('i');
-//     done.classList.add('bi', 'bi-check-square-fill', 'i-done');
-//     // Done
-//     done.addEventListener('click', changeTaskState)
-
-//     let eliminate = document.createElement('i');
-//     eliminate.classList.add('bi', 'bi-trash3-fill', 'i-eliminate');
-//     // Eliminate
-//     eliminate.addEventListener('click', eliminateTask)
-
-//     icons.append(done, eliminate);
-
-//     tasksContainer.prepend(task);
-
-//     event.target.reset();
-//     // LIST.push({
-//     //     task: taskText.textContent,
-//     //     id: id,
-//     // });
-//     // console.log(LIST);
-// }
-
-// const eliminateTask = event => {
-//     let task = event.target.parentNode.parentNode;
-//     task.remove();
-// }
-// const editTask = event => {
-//     if (!event.target.parentNode.classList.contains('done')) {
-//         const task = event.target;
-//         task.classList.add('editing');
-//         task.contentEditable = true;
-//         task.focus()
-//     }
-// }
 
 const order = () => {
     const done = [];
@@ -187,7 +117,7 @@ addTaskForm.addEventListener('submit', (event) => {
         addNewTask(id, task.value, false, false);
         LIST.push({
             id: id,
-            task: task,
+            task: task.value,
             done: false,
             eliminated: false,
         });
@@ -217,11 +147,8 @@ tasksContainer.addEventListener('click', (event) => {
     }
 
     else if (elementData == 'editable') {
-        console.log("Me editan!!!");
+        // console.log("Me editan!!!");
         editTask(element);
-    }
-    else {
-        console.log("Aquí no hago nada :(");
     }
 
     localStorage.setItem('toDoList', JSON.stringify(LIST));
@@ -232,13 +159,9 @@ tasksContainer.addEventListener('focusout', (event) => {
     const elementData = element.attributes.data.value;
 
     if (elementData == 'editable') {
-        console.log("Me editaron con focusout!!!");
+        // console.log("Me editaron con focusout!!!");
         focusOut(element);
     }
-    else {
-        console.log("Aquí no hago nada :(");
-    }
-
     localStorage.setItem('toDoList', JSON.stringify(LIST));
 });
 
@@ -248,14 +171,13 @@ tasksContainer.addEventListener('keydown', (event) => {
         const elementData = element.attributes.data.value;
 
         if (elementData == 'editable') {
-            console.log("Presionaron enter!!!");
+            // console.log("Presionaron enter!!!");
             skipJump(element);
         }
 
         localStorage.setItem('toDoList', JSON.stringify(LIST));
     }
 });
-
 
 // Local Storage
 let data = localStorage.getItem('toDoList')
@@ -269,26 +191,8 @@ if (data) {
     id = 0
 }
 
-
 function loadList(array) {
     array.forEach(function (item) {
         addNewTask(item.id, item.task, item.done, item.eliminated)
     })
 }
-
-
-
-
-// let data = localStorage.getItem('toDoList');
-// if (data) {
-//     LIST = JSON.parse(data);
-//     console.log(LIST);
-//     id = LIST.length;
-//     loadList(LIST);
-// }
-
-// function loadList(arr) {
-//     arr.forEach(function (item) {
-//         addNewTask(item.id, item.task, item.done, item.eliminated);
-//     })
-// }

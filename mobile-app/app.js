fb12305eb176c13cb95f8185c44da679776ac112
monicavaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-09-23 20:03:48 +0200

    update shopping-list

--- mobile-app/app.js
+++ mobile-app/app.js
@@ -1,27 +1,75 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
-    dataBaseURL: "https://playground-fda0b-default-rtdb.europe-west1.firebasedatabase.app/",
+    databaseURL: "https://playground-fda0b-default-rtdb.europe-west1.firebasedatabase.app/",
 }
 
 const app = initializeApp(appSettings);
-// const db = getDatabase();
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList")
 
 // Elements
 const inputField = document.querySelector('#input-field');
 const addBtn = document.querySelector('#add-btn');
+const list = document.querySelector('#shopping-list');
 
 // Events
+onValue(shoppingListInDB, (snapshot) => {
+    if (snapshot.exists()) {
+        let itemArray = Object.entries(snapshot.val());
+        clearShoppingList();
+        for (let i = 0; i < itemArray.length; i++) {
+            let currentItem = itemArray[i];
+            appendItemToList(currentItem);
+        }
+    } else {
+        list.innerHTML = '<div class="container"><strong>No items here... yet.</strong></div>';
+    }
+});
+
 addBtn.addEventListener('click', () => {
     let inputValue = inputField.value;
-    console.log(`${inputValue} added to database`);
-    inputField.value = '';
+    if (inputValue) {
+        push(shoppingListInDB, inputValue);
+        clearInputField();
+    }
 });
 
 inputField.addEventListener('keydown', (event) => {
     if (event.key == 'Enter') {
         let inputValue = inputField.value;
-        console.log(`${inputValue} added to database`);
-        inputField.value = '';
+        if (inputValue) {
+            push(shoppingListInDB, inputValue);
+            clearInputField();
+        }
     }
 });
+
+// Funtions
+function appendItemToList(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let itemEl = document.createElement('div');
+    itemEl.classList.add('list-item');
+    itemEl.textContent = itemValue;
+
+    itemEl.addEventListener('dblclick', () => {
+        deleteItem(itemID);
+    });
+
+    list.appendChild(itemEl);
+}
+
+function clearInputField() {
+    inputField.value = '';
+}
+function clearShoppingList() {
+    list.innerHTML = '';
+}
+
+function deleteItem(itemID) {
+    let exactLocationInDB = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationInDB);
+}
---
+++ mobile-app/scrimba.js
@@ -0,0 +1,8 @@
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "lupe@scrimba.com",
+    "02": "pedro@scrimba.com",
+}
+let scrimbaUsersID = Object.keys(scrimbaUsers);
+let scrimbaUsersEmail = Object.values(scrimbaUsers);
+let scrimbaUsersEntries = Object.entries(scrimbaUsers);
